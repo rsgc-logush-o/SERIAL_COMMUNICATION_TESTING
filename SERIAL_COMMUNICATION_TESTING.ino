65af970e3c1b923c72145db40bf014a5ee618f06
rsgc-logush-o <ologush@rsgc.on.ca>
2015-11-29 17:19:27 -0500

    This is simple serial communication with processing
    This will display what is drawn in processing on a led bar graph. This
    is to test simple serial communication with processing.

---
+++ SERIAL_COMMUNICATION_TESTING.ino
@@ -0,0 +1,44 @@
+int val; //THIS HOLDS WHAT TO DISPLAY
+void setup() {
+  
+  //CONFIGURING ALL OF THE PINS
+  PORTD_PCR0 = (1 << 8);
+  PORTD_PCR1 = (1 << 8);
+  PORTD_PCR2 = (1 << 8);
+  PORTD_PCR3 = (1 << 8);
+  PORTD_PCR4 = (1 << 8);
+  PORTD_PCR5 = (1 << 8);
+  PORTD_PCR6 = (1 << 8);
+  PORTD_PCR7 = (1 << 8);
+
+  GPIOD_PDDR = B11111111;
+
+  //STARTING SERIAL COMMUNICATION
+  Serial.begin(9600);
+
+  //MAKE SURE THAT THE ARDUINO IS CONNECTED
+  establishContact();
+}
+
+void loop() {
+
+  //UPDATING THE VALUE TO DISPLAY IF SOMETHING IS SENT
+  if(Serial.available() > 0)
+  {
+    val = Serial.read(); 
+  }
+
+  //SETTING THE PINS TO THE VALUE
+  GPIOD_PDOR = val;
+
+}
+
+void establishContact()//THIS FUNCTION LOOPS UNTIL PROCESSING SENDS A VALUE BACK TO CONFIRM A SERIAL CONNECTION HAS HAPPENED
+{
+  while(Serial.available() <= 0)
+  {
+    Serial.println("Y");
+    delay(200);
+  }
+}
+
